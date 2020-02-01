e731e8f08ef9a1cdd840c721bbcf0f5994b04d00
Shadi Hortamani <shadi.hortamani@gmail.com>
2020-02-01 14:51:40 -0800

    detect param

--- src/main/java/com/example/slackbot/DemoApplication.java
+++ src/main/java/com/example/slackbot/DemoApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -12,7 +13,7 @@ public class DemoApplication {
 
     @RequestMapping
     @ResponseBody
-    String home() {
+    String home(@RequestParam String text) {
         return "Hey Slackers!";
     }
 
